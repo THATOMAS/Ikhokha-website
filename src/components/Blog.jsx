9566e6a8a779440346031c759d5f1f0e7f450d82
THATOMAS <chachumashori@gmail.com>
2022-08-18 04:55:58 +0200

    Created the Blog Posts section and added a functional button that lets you see more or less blog posts on click

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React,{useState} from "react"
 import styled from "styled-components"
 import {Logos} from "./Logos"
+import {RowOne,RowTwo} from "./BlogData"
 
 const BlogContainer = styled.article`
 height:100vh;
@@ -59,11 +60,96 @@ const BlogBoxContainer = styled.div`
 height:75%;
 width:100%;
 // background:yellow;
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+`
+const ReadMoreButton = styled.div`
+height:38px;
+width:140px;
+background: #FFFFFF 0% 0% no-repeat padding-box;
+border: 1px solid #3D86BF;
+border-radius: 32px;
+opacity: 1;
+cursor:pointer;
+margin-top:20px;
+display:flex;
+justify-content:center;
+align-items:center;
+`
 
+const ReadMoreText = styled.p`
+font: normal normal 600 14px/18px Montserrat;
+letter-spacing: 0px;
+color: #4285BC;
+opacity: 1;
+`
+
+const BlogBoxWrapper = styled.div`
+height:fit-content;
+width:fit-content;
+// background:green;
+display:flex;
+flex-direction:column;
+`
+
+
+const BlogBoxRowOne = styled.div`
+height:200px;
+width:100%;
+// background:blue;
+margin-bottom:10px;
+display:flex;
+flex-direction:row;
+flex-wrap:no-wrap;
+`
+
+const SingleBlogBox = styled.div`
+width:282px;
+height:200px;
+// background:red;
+margin-right:5px;
+display:flex;
+flex-direction:column;
+
+`
+
+const BlogImage = styled.div`
+height:144px;
+width:100%;
+background: url(${props=>props.image})
+`
+
+const Title = styled.h4`
+font-weight:600;
+font-size:20px;
+font-family:Montserrat;
+margin-top:24px;
+margin-bottom:24px;
+color: #394E5D;
+opacity: 1;
+`
+
+const BlogBoxRowTwo = styled.div`
+height:200px;
+width:100%;
+// background:purple;
+margin-bottom:10px;
+display:${props=>props.seemore ? "flex" : "none"};
+flex-direction:row;
+flex-wrap:no-wrap;
 `
 
 
 const Blog = ({titleRef3})=>{
+
+	const [seemore,setSeemore] = useState(true)
+
+	const readmore = ()=>{
+		setSeemore(!seemore)
+	}
+
 	return(
 		<BlogContainer ref={titleRef3}>
 			<PageWrapper>
@@ -78,6 +164,30 @@ const Blog = ({titleRef3})=>{
 					</LogoBox>
 				</TrustedByBox>
 				<BlogBoxContainer>
+					<BlogBoxWrapper>
+						<BlogBoxRowOne>
+							{RowOne.map((blog,index)=>{
+								return(
+									<SingleBlogBox key={index}>
+										<BlogImage image={blog.image}/>
+										<Title>{blog.Title} #{index+1}</Title>
+									</SingleBlogBox>
+									)
+							})}
+						</BlogBoxRowOne>
+						<BlogBoxRowTwo seemore={seemore}>
+							{RowTwo.map((blog,index)=>{
+								return(
+									<SingleBlogBox key={index}/>
+									)
+							})}
+						</BlogBoxRowTwo >
+					</BlogBoxWrapper>
+					<ReadMoreButton onClick={()=>readmore()}>
+						<ReadMoreText>
+							{seemore ? "See Less" : "Read More"}
+						</ReadMoreText>
+					</ReadMoreButton>
 				</BlogBoxContainer>
 			</PageWrapper>
 		</BlogContainer>
---
+++ src/components/BlogData.jsx
@@ -0,0 +1,51 @@
+export const RowOne = [{
+	image:require("../assets/dustin.png"),
+	Title : "Blog Title",
+	info:"Blog excerpt - first lines - for approx two lines"
+}
+,
+{
+	image:require("../assets/dustin.png"),
+	Title : "Blog Title",
+	info:"Blog excerpt - first lines - for approx two lines"
+}
+,
+{
+	image:require("../assets/dustin.png"),
+	Title : "Blog Title",
+	info:"Blog excerpt - first lines - for approx two lines"
+}
+,
+{
+	image:require("../assets/dustin.png"),
+	Title : "Blog Title",
+	info:"Blog excerpt - first lines - for approx two lines"
+}
+
+]
+
+export const RowTwo = [{
+	image:require("../assets/dustin.png"),
+	Title : "Blog Title",
+	info:"Blog excerpt - first lines - for approx two lines"
+}
+,
+{
+	image:require("../assets/dustin.png"),
+	Title : "Blog Title",
+	info:"Blog excerpt - first lines - for approx two lines"
+}
+,
+{
+	image:require("../assets/dustin.png"),
+	Title : "Blog Title",
+	info:"Blog excerpt - first lines - for approx two lines"
+}
+,
+{
+	image:require("../assets/dustin.png"),
+	Title : "Blog Title",
+	info:"Blog excerpt - first lines - for approx two lines"
+}
+
+]
